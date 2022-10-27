18c2901c69dc9a4afa4a07dd5981f931a382c017
Corentin Lebarilier <corentin.lebarilier@epita.fr>
2022-10-27 17:29:00 +0200

    first commit

---
+++ test_SDL/SDL_Test/build/CMakeFiles/SDLTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLTest.dir/Main.o"
+  "CMakeFiles/SDLTest.dir/Main.o.d"
+  "SDLTest"
+  "SDLTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
